9d09b7c5fbb703918714515bb79bf7a3bb88c982
CaesarYang <CaesarYangs@outlook.com>
2021-01-25 22:07:20 +0800

    Initial Commit

---
+++ Intro/IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroApp.swift
+//  Intro
+//
+//  Created by 杨业卿 on 2021/1/25.
+//
+
+import SwiftUI
+
+@main
+struct IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
